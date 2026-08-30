7e788906bc01a4b4a7e1ab0e9b754feb8d9d45de
Cathy Lefebvre <cathy.e.lefebvre@gmail.com>
2026-08-30 18:17:20 -0400

    add terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "owner" {
+  type    = string
+  default = "Cathy-Codes"
+}
+
+variable "project_name" {
+  type    = string
+  default = "dice-game"
+}
+
+variable "region" {
+  description = "the AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment name"
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be dev, staging, or prod."
+  }
+}
+
+
+locals {
+    repo_bucket =  "${var.project_name}-${var.environment}-repo" 
+}
